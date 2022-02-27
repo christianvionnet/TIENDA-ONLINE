6558d6d4c9d16770bd0f205cd1d119f3233d940c
christianvionnet <christvionnet@gmail.com>
2022-02-27 18:06:04 -0300

    Se agrego sección de sucursales y otros cambios

--- js/app2.js
+++ js/app2.js
@@ -1,19 +1,34 @@
 const sticky = window.pageYOffset;
-const beerItemsBody = document.querySelector("#beer-items");
-const pizzaItemsBody = document.querySelector("#pizza-items");
-const modalContainer = document.querySelector("#modal-container");
-const modalItems = document.querySelector("#modal__template--items").content;
-const beerTemplate = document.querySelector("#beer__template").content;
-const pizzaTemplate = document.querySelector("#pizza__template").content;
-const modalTemplate = document.querySelector("#modal__template").content;
+const beerItemsBody = document.querySelector("#beer-items"); //Container
+const pizzaItemsBody = document.querySelector("#pizza-items"); //Container
+const modalContainer = document.querySelector("#modal-container"); //Container
+const branchItemsBody = document.querySelector("#branch-items"); //Container
+const modalItems = document.querySelector("#modal__template--items").content; //Template
+const beerTemplate = document.querySelector("#beer__template").content; //Template
+const pizzaTemplate = document.querySelector("#pizza__template").content; //Template
+const modalTemplate = document.querySelector("#modal__template").content; //Template
+const branchTemplate = document.querySelector("#branch__template").content; //Template
 const navbar = document.querySelector(".nav");
 const fragment = document.createDocumentFragment();
 const buttonSendOrder = document.querySelector("#button-send-order");
+const buttonConfirmOrder = modalTemplate.querySelector("#button-confirm-order");
 const modalTBody = modalTemplate.querySelector("#modal__tbody");
 const modalClose = modalTemplate.querySelector(".modal__close");
-// const buttonModifyOrder = document
+const landingModal = modalTemplate.querySelector(".landing__modal");
+const branchInfo = branchTemplate.querySelectorAll(".branch__info");
 
 let newCart = {}; // It will be our objects collection
+
+// document.addEventListener("scroll", () => {
+//   if (scrollY > 4800 && scrollY < 5200) {
+//     console.log(branchInfo[1]);
+
+//     if (branchInfo[0].classList.contains("hidden")) {
+//       branchInfo[0].classList.remove("hidden");
+//     }
+//     branchInfo[0].classList.add("hidden");
+//   }
+// });
 
 window.onscroll = () => {
   if (window.pageYOffset > 640) {
@@ -25,6 +40,10 @@ window.onscroll = () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchData();
+  if (localStorage.getItem("finalOrder")) {
+    newCart = JSON.parse(localStorage.getItem("finalOrder"));
+    renderModal();
+  }
 });
 
 beerItemsBody.addEventListener("click", (e) => addCart(e));
@@ -34,35 +53,44 @@ pizzaItemsBody.addEventListener("click", (e) => {
 
 buttonSendOrder.addEventListener("click", (e) => {
   e.preventDefault();
-  let newCartJson = JSON.stringify(newCart);
-  localStorage.setItem("finalOrder", newCartJson);
+
+  localStorage.setItem("finalOrder", JSON.stringify(newCart));
+
   renderModal();
 });
 
+buttonConfirmOrder.addEventListener("click", () => {
+  localStorage.removeItem("finalOrder");
+  window.alert("¡Listo! Último paso, vamos al pago");
+});
+
 modalClose.addEventListener("click", (e) => {
-  // console.log(e.target.parentElement.parentElement);
-  e.target.parentElement.parentElement.style.display = "none";
+  landingModal.style.visibility = "hidden";
 });
 
 //Function for retreiving data from APIs
 const fetchData = async () => {
   try {
-    const res = await fetch("/api.json");
-    const data = await res.json();
+    const beerAPI = await fetch("/api.json");
+    const beerData = await beerAPI.json();
 
-    const res2 = await fetch("/api2.json");
-    const data2 = await res2.json();
+    const pizzaAPI = await fetch("/api2.json");
+    const pizzaData = await pizzaAPI.json();
 
-    renderItems(data);
-    renderItems2(data2);
+    const branchAPI = await fetch("/api3.json");
+    const branchData = await branchAPI.json();
+
+    renderItems(beerData);
+    renderItems2(pizzaData);
+    renderBranches(branchData);
   } catch (error) {
     console.log(error);
   }
 };
 
 //Function for rendering beer items on the html body
-const renderItems = (data) => {
-  data.forEach((product) => {
+const renderItems = (beerData) => {
+  beerData.forEach((product) => {
     beerTemplate.querySelector(".landing__style").textContent = product.title;
     beerTemplate.querySelector(".landing__info").textContent = product.info;
     beerTemplate.querySelector(".landing__price").textContent = product.price;
@@ -77,8 +105,8 @@ const renderItems = (data) => {
 };
 
 //Function for rendering pizza items on the html body
-const renderItems2 = (data2) => {
-  data2.forEach((product) => {
+const renderItems2 = (pizzaData) => {
+  pizzaData.forEach((product) => {
     pizzaTemplate.querySelector(".landing__style").textContent = product.title;
     pizzaTemplate.querySelector(".landing__info").textContent = product.info;
     pizzaTemplate.querySelector(".landing__price").textContent = product.price;
@@ -90,14 +118,34 @@ const renderItems2 = (data2) => {
   pizzaItemsBody.appendChild(fragment);
 };
 
+const renderBranches = (branchData) => {
+  branchData.forEach((branch) => {
+    branchTemplate.querySelector("h3").textContent = branch.title;
+    branchTemplate.querySelector("p").textContent = branch.address;
+    branchTemplate.querySelector("a").setAttribute("src", branch.map);
+    branchTemplate.querySelector("img").setAttribute("src", branch.img);
+    const clone = branchTemplate.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  branchItemsBody.appendChild(fragment);
+};
+
 //Fuction for taking the final order and rendering the modal with it
 const renderModal = () => {
   let finalOrderJson = localStorage.getItem("finalOrder");
   let finalOrder = JSON.parse(finalOrderJson);
-  // console.log(modalTemplate);
-  // if (modalTemplate) {
+
   modalTemplate.querySelector(".modal__total").textContent = "0";
-  // }
+  if (Object.values(finalOrder).length == 0) {
+    console.log("carrito vacío");
+    let emptyCartMessage = document.createElement("div");
+    emptyCartMessage.textContent = "¡UPS! TU CARRITO ESTÁ VACÍO";
+    emptyCartMessage.style.padding = "2em 0";
+    modalTBody.appendChild(emptyCartMessage);
+    modalContainer.appendChild(modalTemplate);
+
+    return;
+  }
 
   Object.values(finalOrder).forEach((product) => {
     modalItems.querySelector(".modal__item").textContent = product.title;
@@ -106,9 +154,11 @@ const renderModal = () => {
       product.price * product.quantity;
     const clone = modalItems.cloneNode(true);
 
-    modalTemplate.querySelector(".modal__total").textContent =
-      parseInt(modalTemplate.querySelector(".modal__total").textContent) +
-      parseInt(modalItems.querySelector(".modal__price").textContent);
+    const total = Object.values(finalOrder).reduce(
+      (acc, { quantity, price }) => acc + quantity * price,
+      0
+    );
+    modalTemplate.querySelector(".modal__total").textContent = total;
     fragment.appendChild(clone);
   });
   modalTBody.appendChild(fragment);
@@ -120,7 +170,8 @@ const renderModal = () => {
 const addCart = (e) => {
   if (e.target.classList.contains("landing__add")) {
     setCartPlus(e.target.parentElement.parentElement.parentElement);
-  } else if (e.target.classList.contains("landing__substract")) {
+  }
+  if (e.target.classList.contains("landing__substract")) {
     setCartLess(e.target.parentElement.parentElement.parentElement);
   }
   e.stopPropagation();
@@ -144,9 +195,6 @@ const setCartPlus = (objeto) => {
   }
 
   newCart[product.id] = { ...product };
-
-  // console.log(newCart);
-  // renderModal(newCart);
 };
 
 // Function for substracting an item to the cart
@@ -161,6 +209,7 @@ const setCartLess = (objeto) => {
   if (newCart.hasOwnProperty(product.id)) {
     if (newCart[product.id].quantity == 0) {
       product.quantity = newCart[product.id].quantity;
+      delete newCart[product.id];
     } else {
       product.quantity = newCart[product.id].quantity - 1;
       objeto.querySelector(".landing__input").value =
@@ -173,5 +222,3 @@ const setCartLess = (objeto) => {
   // console.log(newCart);
   // renderModal(newCart);
 };
-
-const openCart = () => {};
